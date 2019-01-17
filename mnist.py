5a2be92c1d324c071380739eb3cbc5a9c1d26242
Admin <admin@Admins-MBP.fios-router.home>
2019-01-16 20:40:47 -0500

    add precision recall

--- mnist.py
+++ mnist.py
@@ -26,4 +26,21 @@ y=mnist['label'].transpose()
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
+y_train_5 = (y_train==5)
+
+from sklearn.linear_model import SGDClassifier
+sgd = SGDClassifier(random_state=42)
+
+from sklearn.model_selection import cross_val_predict
+y_train_predict = cross_val_predict(sgd, X_train, y_train_5, cv=3)
+
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_train_5, y_train_predict)
+
+from sklearn.metrics import precision_recall_curve
+y_score = cross_val_predict(sgd, X_train, y_train_5,cv=3, method='decision_function')
+precision,recall,threshold = precision_recall_curve(y_train_5, y_score)
+
 pass
